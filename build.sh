b990cff5f737dc04879aced304704e2403c54a97
Austin Mackey <rangerair@Austins-MacBook-Air.local>
2025-03-09 12:04:20 -0400

    update build script to delete lib files before copying over from source.

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+ttydevice=$(ls /dev/tty.usbmodem*)
+
 mpremote fs cp --force ./rootdir/* :
+(ampy -p $ttydevice ls /lib | sed 's|^/|:|' | xargs -I {} mpremote fs rm {})
 mpremote fs cp --force ./rootdir/lib/* :lib
 mpremote exec --no-follow "import microcontroller; microcontroller.reset()"
