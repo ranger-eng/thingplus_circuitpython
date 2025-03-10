dc9777636c7ec9cfa65b7363f0197c5f1a6b3b17
Austin Mackey <rangerair@Austins-MacBook-Air.local>
2025-03-10 09:29:21 -0400

    this one actually transmits the data over ble, it is just the example code but it works. Bugus data tho.

--- rootdir/code.py
+++ rootdir/code.py
@@ -11,61 +11,26 @@ from adafruit_ble import BLERadio
 from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
 
 
-
-# setup sensors
-import board
-import busio
-import sensors
-
-# Measure analog moisture levels
-# Initialize analog input pins
-moistSensor1 = sensors.MoistureSensor(board.PA0)
-moistSensor2 = sensors.MoistureSensor(board.PA4)
-comm_port = busio.I2C(board.PD0, board.PD1)
-
 # Create BLE radio, custom service, and advertisement.
 ble = BLERadio()
 service = SensorService()
 advertisement = ProvideServicesAdvertisement(service)
 
-uvSensor = sensors.UVSensor(comm_port, 0x74)
 
-def clear_screen():
-    """Clears the terminal output"""
-    print("\033[2J\033[H", end="")
+# Function to get some fake weather sensor readings for this example in the desired unit.
+def measure(unit):
+    temperature = random.uniform(0.0, 10.0)
+    humidity = random.uniform(0.0, 100.0)
+    if unit == "fahrenheit":
+        temperature = (temperature * 9.0 / 5.0) + 32.0
+    return {"temperature": temperature, "humidity": humidity}
 
-def measure():
-    m1_val = moistSensor1.getVoltage()
-    m2_val = moistSensor2.getVoltage()
-    uva_val, _, _, temp_val = uvSensor.values
-    return {"moisture1": m1_val, "moisture2": m2_val, "uva": uva_val, "air_temp": temp_val}
-
-# Main loop to read and print analog values
-while True:
-    print("Advertise services")
-    ble.stop_advertising()  # you need to do this to stop any persistent old advertisement
-    ble.start_advertising(advertisement)
-
-    print("Waiting for connection...")
-    while not ble.connected:
-        pass
-
-    print("Connected")
-    while ble.connected:
-        measurement = measure()
-        service.sensors = measurement
-        print("Characteristics: ", service.bleio_characteristics)
-        print("Sensors: ", service.sensors)
-        time.sleep(3)
-
-    print("Disconnected")
 
 # Advertise until another device connects, when a device connects, provide sensor data.
 while True:
     print("Advertise services")
     ble.stop_advertising()  # you need to do this to stop any persistent old advertisement
     ble.start_advertising(advertisement)
-    print(advertisement.match_prefixes)
 
     print("Waiting for connection...")
     while not ble.connected:
